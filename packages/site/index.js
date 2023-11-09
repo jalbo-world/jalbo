f413b6d82eed2cf2042ba3c35ffaa6c634b924f1
Kelvin Fichter <kelvin@optimism.io>
2023-11-08 22:43:27 -0500

    feat: use attestations

--- packages/site/index.js
+++ packages/site/index.js
@@ -1,50 +1,105 @@
-import { ethers } from 'https://cdnjs.cloudflare.com/ajax/libs/ethers/5.7.2/ethers.esm.min.js'
-
 // Configuration
-const CONFIG_ADDRESS = '0xcbebc5ba53ff12165239cbb3d310fda2236d6ad2'
+const CONFIG_MANAGER_SCHEMA = '0x81ba1506292bcde4ceae1d8960f26f2e62c9c61b7ecbe3cd3d73682453fa8b7c'
+const CONFIG_RECORD_SCHEMA = '0x245b64ff9792a92c3026895d09423841ca1e772e877c42d3a3159a268c7ffec2'
 const CONFIG_ADMIN = '0x4F24f7cF6Bfc7F6A00a10d4d5AB6a5296a1416d8'
-const CONFIG_KEY = 'jalbo'
-const CONFIG_NETWORK = 'goerli'
 const CONFIG_BACKUP = 'https://en.wikipedia.org/wiki/Human_error'
-const CONFIG_ABI = '[{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"string","name":"","type":"string"}],"name":"config","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"key","type":"string"},{"internalType":"string","name":"val","type":"string"}],"name":"update","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
 
 /**
- * Grabs a config value from the Configuraton contract.
+ * Performs a GraphQL request to EAS.
  * 
- * @param {*} user     User that set the config value.
- * @param {*} key      Key of the config value.
- * @param {*} fallback Fallback value if the config value is not set.
+ * @param {*} schema   Schema ID to match against.
+ * @param {*} attester Attester to match against.
+ * @param {*} content  Content to match against.
  * 
- * @returns Config value or fallback.
+ * @returns Result of the GraphQL request.
  */
-const config = async (user, key, fallback) => {
-  const contract = new ethers.Contract(
-    CONFIG_ADDRESS,
-    CONFIG_ABI,
-    new ethers.providers.InfuraProvider(CONFIG_NETWORK)
-  )
-
-  let res = undefined
-  for (let i = 0; i < 5; i++) {
-    try {
-      res = await contract.config(user, key)
-      break
-    } catch (err) {
-      console.log(err)
-      await sleep(1000)
+const request = async (schema, attester, content) => {
+  return (await fetch(
+    'https://optimism.easscan.org/graphql',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query Query($where: AttestationWhereInput, $take: Int) {
+            attestations(where: $where, take: $take) {
+              id
+              schemaId
+              attester
+              recipient
+              decodedDataJson
+            }
+          }
+        `,
+        variables: {
+          take: 1,
+          where: {
+            schemaId: {
+              equals: schema,
+            },
+            attester: {
+              equals: attester,
+            },
+            decodedDataJson: {
+              contains: content
+            },
+            revoked: {
+              equals: false,
+            }
+          }
+        },
+      }),
     }
-  }
-
-  return res || fallback
+  )).json()
 }
 
 /**
- * Grabs the current Jalbo link.
+ * Grabs the manager for a specific subdomain.
  * 
- * @returns Jalbo link.
+ * @param {*} subdomain Subdomain to grab the manager for.
+ * 
+ * @returns Manager for the given subdomain.
  */
-const jalbo = async () => {
-  return config(CONFIG_ADMIN, CONFIG_KEY, CONFIG_BACKUP)
+const manager = async (subdomain) => {
+  return (await request(
+    CONFIG_MANAGER_SCHEMA,
+    CONFIG_ADMIN,
+    subdomain
+  )).data.attestations[0].recipient
+}
+
+/**
+ * Grabs the Jalbo link for a specific subdomain.
+ * 
+ * @param {*} subdomain Subdomain to grab the Jalbo link for.
+ * 
+ * @returns Jalbo link for the given subdomain.
+ */
+const record = async (subdomain) => {
+  try {
+    return JSON.parse((await request(
+      CONFIG_RECORD_SCHEMA,
+      await manager(subdomain),
+      subdomain
+    )).data.attestations[0].decodedDataJson)[1].value.value
+  } catch (err) {
+    return CONFIG_BACKUP
+  }
+}
+
+/**
+ * Gets the subdomain for the current page.
+ * 
+ * @returns Subdomain for the current page.
+ */
+const subdomain = () => {
+  if (window.location.hostname.includes('jalbo.link')) {
+    return window.location.hostname.split('.')[0]
+  } else {
+    return 'jalbo'
+  }
 }
 
 /**
@@ -73,7 +128,7 @@ const sleep = (ms) => {
 const main = async () => {
   console.log('Grabbing Jalbo link...')
   const tick = Date.now()
-  const link = await jalbo()
+  const link = await record(subdomain())
   const tock = Date.now() - tick
   console.log(`Grabbed Jalbo link in ${tock}ms`)
   await sleep(Math.max(0, 1000 - tock))
